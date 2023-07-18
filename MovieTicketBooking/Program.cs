c38c62d49c5208067477d82922038695e11e2b7e
ashlyvarghese25 <ashlyvarghese1025@gmail.com>
2023-07-18 11:25:54 +0530

    MovieTicketBookingSwagger

--- MovieTicketBooking/Program.cs
+++ MovieTicketBooking/Program.cs
@@ -111,6 +111,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors("EnableCors");
 app.UseHttpsRedirection();
 
